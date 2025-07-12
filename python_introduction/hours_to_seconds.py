94ce8a4e171fd46c2f52b0e2059c8f3bcb4ab943
Michael Okang <michaelokang8@gmail.com>
2025-07-12 22:01:01 +0000

    Add files via upload

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,10 @@
+seconds = 60
+A_minute = seconds
+minutes = 60
+hour = minutes
+hours = 2 
+minutes_to_hours = minutes * hours
+hours_to_seconds = minutes_to_hours * seconds
+
+print(hours, "hours is", hours_to_seconds, "seconds")
+
