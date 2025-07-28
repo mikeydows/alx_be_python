edf4b4bcc2bb9b9e8fff19d04cb3d2e91e6dcbdf
Michael Okang <michaelokang8@gmail.com>
2025-07-28 23:59:43 +0000

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -6,7 +6,6 @@ def display_menu():
     print("3. View the items")
     print("4. Exit")
 
-
 def main():
     while True:
         display_menu()
@@ -25,19 +24,4 @@ def main():
             remove_item = input("Enter the item to remove: ")
             if remove_item in shopping_list:
                 shopping_list.remove(remove_item)
-                print("Item removed.")
-            else:
-                print(f"'{remove_item}' is not in the shopping list.")
-            print(shopping_list)
-
-        elif choice == 3:
-            print("Shopping list:", shopping_list)
-
-        elif choice == 4:
-            break
-
-        else:
-            print("Choose a valid option between 1 and 4.")
-
-if __name__ == "__main__":
-    main()
+                print("Item remov
