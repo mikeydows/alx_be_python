22545ba636cb9eaccb46b7c0f1effd2c15732389
Michael Okang <michaelokang8@gmail.com>
2025-08-11 21:24:48 +0000

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,14 +8,14 @@ class Book:
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super().__init__(title, author)
-        self.file_size = file_size
+        super().__init__(title, author)  # Call base class constructor
+        self.file_size = file_size       # Unique to EBook
 
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super().__init__(title, author)
-        self.page_count = page_count
+        super().__init__(title, author)  # Call base class constructor
+        self.page_count = page_count     # Unique to PrintBook
 
 
 class Library:
